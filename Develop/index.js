853b9c318b6a06bd950e466a893ffa85be90ebee
yaseminvatan <yvatan035@gmail.com>
2024-09-26 14:42:38 -0700

    Necessary packages included imported

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require('fs'); //its a node.js module helpd read write and manupulates the file. so my code can write to file from user input. we need this 
+// i also need to ask question to user so i will use inquirer module to ask questions.
+const inquirer = require ('inquirer');
+// and i need generateMarkdown under utils folder to generate Markdown like formattion the text for readme file.
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// TODO: Create an array of questions for user input.
 const questions = [];
 
 // TODO: Create a function to write README file
