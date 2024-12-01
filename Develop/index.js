c06b9a7d0576c4c562053f7c78647ade59f5429e
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-01 06:46:34 -0800

    feat: add prompt script for project information and license selection

--- Develop/index.js
+++ Develop/index.js
@@ -13,6 +13,47 @@ const questions = [
         message: 'what is the title of your project'
 
     }
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },   
 ];
 
 // TODO: Create a function to write README file
